e035f7de0f97cd696de63f23584245b6b9019965
Hardi <hardivirani2003@gmail.com>
2025-11-03 17:01:53 -0500

    First_Day

--- src/main/java/guru/springframework/spring5webapp/Foo.java
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -6,6 +6,6 @@ public class Foo
 {
     public static  void main(String[] args)
     {
-        System.out.println("Hardi Virani - Day One");
+        System.out.println("Hardi - Day One");
     }
 }
