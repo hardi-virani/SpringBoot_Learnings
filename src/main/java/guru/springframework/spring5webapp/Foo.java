bf82df25e8bb003319fbaf41db70eb73b3a09014
Hardi <hardivirani2003@gmail.com>
2025-11-03 17:00:11 -0500

    DAY_One

---
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+
+public class Foo
+{
+    public static  void main(String[] args)
+    {
+        System.out.println("Hardi");
+    }
+}
